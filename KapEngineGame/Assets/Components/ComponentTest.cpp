cc9c3f84f8cd2cdd9f86e9dd49e7d7aae8f389c0
benji-35 <benjamin.delvert@epitech.eu>
2022-09-20 15:52:40 +0200

    CHG : add test game component

---
+++ KapEngineGame/Assets/Components/ComponentTest.cpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2022
+** gameEngine2
+** File description:
+** ComponentTest
+*/
+
+#include "ComponentTest.hpp"
+#include "AsyncOperation.hpp"
+#include "Debug.hpp"
+
+MyGame::ComponentTest::ComponentTest(std::shared_ptr<KapEngine::GameObject> &go, int threadId) : KapEngine::Component(go, "TextComp", threadId)
+{
+    _thread = threadId;
+}
+
+MyGame::ComponentTest::~ComponentTest()
+{
+}
+
+void MyGame::ComponentTest::onStart() {
+    std::any comp = this;
+    KapEngine::Operation::AsyncOperation async(comp);
+
+    async.setOperationFunction([this](std::any an){
+        std::cout << this->getName() << std::endl;
+    });
+    async.startAsync();
+}
