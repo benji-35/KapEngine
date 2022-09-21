8c2df2f6c02160ddebbce10c45ae3e060058d4c0
Chaika9 <bestdevelop22@gmail.com>
2022-09-21 15:44:01 +0200

    FIX : engine_inludes.cmake relative path

--- CMake/engine_includes.cmake
+++ CMake/engine_includes.cmake
@@ -1,34 +1,34 @@
 # Engine includes
-include_directories(KapEngine/Components)
-include_directories(KapEngine/Components/Transform)
-include_directories(KapEngine/Components/Camera)
+include_directories(${KapEngine_SOURCE_DIR}/KapEngine/Components)
+include_directories(${KapEngine_SOURCE_DIR}/KapEngine/Components/Transform)
+include_directories(${KapEngine_SOURCE_DIR}/KapEngine/Components/Camera)
 
-include_directories(KapEngine/Component)
-include_directories(KapEngine/GameObject)
+include_directories(${KapEngine_SOURCE_DIR}/KapEngine/Component)
+include_directories(${KapEngine_SOURCE_DIR}/KapEngine/GameObject)
 
-include_directories(KapEngine/Engine)
-include_directories(KapEngine/Errors)
+include_directories(${KapEngine_SOURCE_DIR}/KapEngine/Engine)
+include_directories(${KapEngine_SOURCE_DIR}/KapEngine/Errors)
 
-include_directories(KapEngine/SceneManagement/Scene)
-include_directories(KapEngine/SceneManagement/SceneManager)
+include_directories(${KapEngine_SOURCE_DIR}/KapEngine/SceneManagement/Scene)
+include_directories(${KapEngine_SOURCE_DIR}/KapEngine/SceneManagement/SceneManager)
 
-include_directories(KapEngine/Tools)
-include_directories(KapEngine/Debug)
+include_directories(${KapEngine_SOURCE_DIR}/KapEngine/Tools)
+include_directories(${KapEngine_SOURCE_DIR}/KapEngine/Debug)
 
-include_directories(KapEngine/GraphicalLib)
+include_directories(${KapEngine_SOURCE_DIR}/KapEngine/GraphicalLib)
 
-include_directories(KapEngine/Events/Input)
-include_directories(KapEngine/Events/Mouse)
-include_directories(KapEngine/Events/Manager)
+include_directories(${KapEngine_SOURCE_DIR}/KapEngine/Events/Input)
+include_directories(${KapEngine_SOURCE_DIR}/KapEngine/Events/Mouse)
+include_directories(${KapEngine_SOURCE_DIR}/KapEngine/Events/Manager)
 
-include_directories(KapEngine/Legacy/Graphical)
+include_directories(${KapEngine_SOURCE_DIR}/KapEngine/Legacy/Graphical)
 
-include_directories(KapEngine/Events/Action)
+include_directories(${KapEngine_SOURCE_DIR}/KapEngine/Events/Action)
 
-include_directories(KapEngine/Time)
+include_directories(${KapEngine_SOURCE_DIR}/KapEngine/Time)
 
-include_directories(KapEngine/Entity)
+include_directories(${KapEngine_SOURCE_DIR}/KapEngine/Entity)
 
-include_directories(KapEngine/SpecialOperation)
-include_directories(KapEngine/SpecialOperation/AsyncOperation)
-include_directories(KapEngine/SpecialOperation/SyncOperation)
+include_directories(${KapEngine_SOURCE_DIR}/KapEngine/SpecialOperation)
+include_directories(${KapEngine_SOURCE_DIR}/KapEngine/SpecialOperation/AsyncOperation)
+include_directories(${KapEngine_SOURCE_DIR}/KapEngine/SpecialOperation/SyncOperation)
