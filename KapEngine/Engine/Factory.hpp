c87efa9e238c6e67ca4f663774582c1776ee2afc
benji-35 <benjamin.delvert@epitech.eu>
2022-09-23 17:35:24 +0200

    CHG ; create scene like SceneManager init

--- KapEngine/Engine/Factory.hpp
+++ KapEngine/Engine/Factory.hpp
@@ -97,9 +97,8 @@ namespace KapEngine {
 
             static std::shared_ptr<SceneManagement::Scene> createScene(SceneManagement::SceneManager &manager, std::string const& name) {
                 auto nScene = std::make_shared<SceneManagement::Scene>(manager, name);
-                manager.addScene(nScene);
-
                 createCamera(nScene, "Main Camera");
+                manager.addScene(nScene);
 
                 return nScene;
             }
