fc9b8022d46edaab2c50055bd78a8d2e52366ec9
benji-35 <benjamin.delvert@epitech.eu>
2022-10-03 14:08:19 +0200

    ADD : includes

---
+++ KapEngine/includes/KapEngine/Graphical.hpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2022
+** engine
+** File description:
+** Graphical
+*/
+
+#ifndef INCLUDES_KAPENGINE_GRAPHICAL_HPP_
+#define INCLUDES_KAPENGINE_GRAPHICAL_HPP_
+
+#include "../../GraphicalLib/GraphicalLib.hpp"
+#include "../../GraphicalLib/GraphicalLibManager.hpp"
+
+#endif /* !INCLUDES_KAPENGINE_GRAPHICAL_HPP_ */
