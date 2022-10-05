16dcbca454225e10440e54434cbcd6b7ca17cdc7
benji-35 <benjamin.delvert@epitech.eu>
2022-10-05 15:49:06 +0200

    ADD : empty collider component

--- CMake/engine_include.cmake
+++ CMake/engine_include.cmake
@@ -9,6 +9,7 @@ include_directories(${KapEngine_SOURCE_DIR}/KapEngine/Components/Animator)
 include_directories(${KapEngine_SOURCE_DIR}/KapEngine/Components/AnimationFadeIn)
 include_directories(${KapEngine_SOURCE_DIR}/KapEngine/Components/AnimationFadeOut)
 include_directories(${KapEngine_SOURCE_DIR}/KapEngine/Components/MouseDetector)
+include_directories(${KapEngine_SOURCE_DIR}/KapEngine/Components/Collider)
 
 ## Component + GameObject
 include_directories(${KapEngine_SOURCE_DIR}/KapEngine/Component)
