a01d0f67577aaea84d9a3338cf71b960d65734cc
benji-35 <benjamin.delvert@epitech.eu>
2022-10-13 15:09:08 +0200

    ADD : first collider version

--- KapEngine/Components/Collider/Collider.cpp
+++ KapEngine/Components/Collider/Collider.cpp
@@ -6,9 +6,176 @@
 */
 
 #include "Collider.hpp"
+#include "KapEngineUi.hpp"
 
 using namespace KapEngine;
 
-Collider::Collider(std::shared_ptr<GameObject> go) : Component(go, "Collider") {}
+Collider::Collider(std::shared_ptr<GameObject> go, bool isTrigger, bool isMovable, bool isCanvas) : Component(go, "Collider"),
+    _isTrigger(isTrigger), _isMovable(isMovable), _isInCanvas(isCanvas) {
+
+}
 
 Collider::~Collider() {}
+
+bool Collider::checkComponentValidity() {
+    if (_isInCanvas) {
+        try {
+            auto &transform = getGameObject().getComponent<Transform>();
+            return transform.getParentContainsComponent("Canvas");
+        } catch (Errors::ComponentError &e) {
+            throw Errors::ComponentError("Collider GameObject does not have a Transform component");
+        }
+    }
+    return true;
+}
+
+void Collider::onUpdate() {
+    auto collision = getCollisionBox();
+    auto gameObjects = getGameObject().getScene().getAllGameObjects();
+    for (std::size_t i = 0; i < gameObjects.size(); i++) {
+        if (gameObjects[i]->getId() != getGameObject().getId()) {
+            calculateCollisions(gameObjects[i]);
+        }
+    }
+    checkNotCollided();
+}
+
+KapEngine::Tools::Vector2 Collider::calculSizeCanvas(KapEngine::Tools::Vector2 const &size) const {
+
+    Tools::Vector2 currentSize = getGameObjectConst().getEngine().getGraphicalLibManager()->getCurrentLib()->getScreenSize();
+    Tools::Vector2 baseSize = getGameObjectConst().getEngine().getScreenSize();
+    Tools::Vector2 result = size;
+
+    try {
+        auto idParent = getGameObjectConst().getComponent<Transform>().getParentContainsComponent("Canvas");
+        auto parent = getGameObjectConst().getScene().getGameObjectConst(idParent);
+
+        auto &canvas = parent->getComponent<UI::Canvas>();
+        baseSize = canvas.getScreenSizeCompare();
+
+    } catch(...) {}
+
+    float xMultiplier = baseSize.getX() / currentSize.getX();
+    float yMultiplier = baseSize.getY() / currentSize.getY();
+
+    result.setX(result.getX() * xMultiplier);
+    result.setY(result.getY() / yMultiplier);
+
+    return result;
+}
+
+void Collider::calculateCollisions(std::shared_ptr<GameObject> object) {
+    if (object->getId() == getGameObject().getId())
+        return;
+
+    auto colliders = object->getComponents<Collider>();
+    for (std::size_t i = 0; i < colliders.size(); i++) {
+        calculateCollisions(colliders[i]);
+    }
+}
+
+void Collider::calculateCollisions(KapEngine::Collider &collider) {
+    //check already calculated
+    for (std::size_t i = 0; i < _calculatedColliders.size(); i++) {
+        if (_calculatedColliders[i].getGameObject().getId() == collider.getGameObject().getId()) {
+            if (_calculatedColliders[i].getId() == collider.getId()) {
+                return;
+            }
+        }
+    }
+    _calculatedColliders.push_back(collider);
+    collider.__addCalculatedCollider(*this);
+    //check collision
+    if (isColliding(collider.getCollisionBox())) {
+        if (_isTrigger) {
+            __callTrigger(collider);
+        }
+        if (collider.isTrigger()) {
+            collider.__callTrigger(*this);
+        }
+    }
+}
+
+bool Collider::isColliding(KapEngine::Tools::Rectangle const rect) {
+    if (_collisionBox.getPos().getX() > rect.getPos().getX() + rect.getSize().getX())
+        return false;
+    if (rect.getPos().getX() > _collisionBox.getPos().getX() + _collisionBox.getSize().getX())
+        return false;
+    if (_collisionBox.getPos().getY() > rect.getPos().getY() + rect.getSize().getY())
+        return false;
+    if (rect.getPos().getY() > _collisionBox.getPos().getY() + _collisionBox.getSize().getY())
+        return false;
+    return true;
+}
+
+void Collider::__callTrigger(Collider &collided) {
+    auto components = getGameObject().getAllComponents();
+    auto goShared = collided.getGameObject().getScene().getGameObject(collided.getGameObject().getId());
+    auto alreadyTriggered = isSavedCollided(collided);
+
+    for (std::size_t i = 0; i < components.size(); i++) {
+        if (alreadyTriggered) {
+            components[i]->onTriggerStay(goShared);
+        } else {
+            components[i]->onTriggerEnter(goShared);
+        }
+    }
+}
+
+void Collider::onSceneUpdated() {
+    checkNotCollided();
+    _calculatedColliders.clear();
+}
+
+void Collider::checkNotCollided() {
+    for (std::size_t i = 0; i < _alreadyCollided.size(); i++) {
+        if (!isSavedCollided(_alreadyCollided[i])) {
+            __callEndTrigger(_alreadyCollided[i]);
+        }
+    }
+}
+
+void Collider::__callEndTrigger(Collider &collided) {
+    auto components = getGameObject().getAllComponents();
+    auto goShared = collided.getGameObject().getScene().getGameObject(collided.getId());
+
+    for (std::size_t i = 0; i < components.size(); i++) {
+        components[i]->onTriggerExit(goShared);
+    }
+    for (std::size_t i = 0; i < _alreadyCollided.size(); i++) {
+        if (_alreadyCollided[i].getId() == collided.getId()) {
+            _alreadyCollided.erase(_alreadyCollided.begin() + i);
+            break;
+        }
+    }
+}
+
+bool Collider::isSavedCollided(Collider &collider) {
+    for (std::size_t i = 0; i < _alreadyCollided.size(); i++) {
+        if (_alreadyCollided[i].getId() == collider.getId()) {
+            return true;
+        }
+    }
+    return false;
+}
+
+Tools::Rectangle Collider::getCollisionBox() const {
+    Tools::Rectangle rect = _collisionBox;
+
+    if (rect.getSize().getX() == 0 || rect.getSize().getY() == 0) {
+        auto &transform = getGameObjectConst().getComponent<Transform>();
+        Tools::Vector2 size;
+        Tools::Vector2 pos;
+        size = transform.getWorldScale();
+        pos = transform.getWorldPosition();
+        rect.setSize(size);
+        rect.setPos(pos);
+    }
+
+    rect.setPos(rect.getPos() + _offset);
+    if (_isInCanvas) {
+        rect.setPos(calculSizeCanvas(rect.getPos()));
+        rect.setSize(calculSizeCanvas(rect.getSize()));
+    }
+    return rect;
+}
--- KapEngine/Components/Collider/Collider.hpp
+++ KapEngine/Components/Collider/Collider.hpp
@@ -15,11 +15,55 @@ namespace KapEngine {
 
     class Collider : public Component {
         public:
-            Collider(std::shared_ptr<GameObject> go);
+            Collider(std::shared_ptr<GameObject> go, bool isTrigger = false, bool isMovable = false, bool isCanvas = false);
             ~Collider();
+
+            void onUpdate() override;
+            void onDisplay() override;
+            bool checkComponentValidity() override;
+            void onSceneUpdated() override;
+
+            void setInCanvas(bool isInCanvas);
+            void setTrigger(bool isTrigger);
+            void setMovability(bool isMovable);
+
+            void setCollisionBox(Tools::Rectangle const& collisionBox);
+
+            void __addCalculatedCollider(Collider &_collider) {
+                _calculatedColliders.push_back(_collider);
+            }
+
+            Tools::Rectangle getCollisionBox() const;
+
+            bool isTrigger() const {
+                return _isTrigger;
+            }
+
+            void __callTrigger(Collider &collided);
+            void __callEndTrigger(Collider &collided);
+
+            void setOffset(Tools::Vector2 const& offset) {
+                _offset = offset;
+            }
 
         protected:
         private:
+            bool _isTrigger = false;
+            bool _isInCanvas = false;
+            bool _isMovable = false;
+            Tools::Rectangle _collisionBox;
+            Tools::Vector2 _offset;
+            std::vector<Collider &> _calculatedColliders;
+            std::vector<Collider &> _alreadyCollided;
+
+            void calculateCollisions(std::shared_ptr<GameObject> object);
+            void calculateCollisions(Collider & collider);
+            void checkNotCollided();
+
+            Tools::Vector2 calculSizeCanvas(Tools::Vector2 const& size) const;
+
+            bool isColliding(Tools::Rectangle const collisionBox);
+            bool isSavedCollided(Collider &collider);
     };
 
 }
