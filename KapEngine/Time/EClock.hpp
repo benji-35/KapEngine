b1b81818b32246955aacf5cd6debbefe70e14624
aureliancnx <aurelien.marcel@epitech.eu>
2022-11-04 15:14:30 +0100

    ADD: profiler for Time objects

--- KapEngine/Time/EClock.hpp
+++ KapEngine/Time/EClock.hpp
@@ -22,10 +22,14 @@ namespace KapEngine {
         class EClock {
             public:
                 EClock() {
+                    PROFILER_FUNC_START();
                     __setNow();
+                    PROFILER_FUNC_END();
                 }
                 EClock(EClock const& _clk) {
+                    PROFILER_FUNC_START();
                     *this = _clk;
+                    PROFILER_FUNC_END();
                 }
                 ~EClock() {}
 
@@ -36,8 +40,10 @@ namespace KapEngine {
                  * @return ETime temps écoulé depuis le dernier redémarrage
                  */
                 virtual ETime restart() {
+                    PROFILER_FUNC_START();
                     ETime _got = __getElapse();
                     __setNow();
+                    PROFILER_FUNC_END();
                     return _got;
                 }
 
@@ -48,11 +54,15 @@ namespace KapEngine {
                  * @return ETime temps écoulé depuis le dernier redémarrage
                  */
                 virtual ETime getElapseTime() {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return ETime(__getElapse());
                 }
 
                 EClock &operator=(EClock const& _clk) {
+                    PROFILER_FUNC_START();
                     this->_start = _clk._start;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
@@ -60,17 +70,21 @@ namespace KapEngine {
                 int64_t _start = 0;
             private:
                 int64_t __getElapse() {
+                    PROFILER_FUNC_START();
                     std::chrono::microseconds ms = std::chrono::duration_cast<std::chrono::microseconds >(
                         std::chrono::high_resolution_clock::now().time_since_epoch()
                     );
                     int64_t _nVal = ms.count();
+                    PROFILER_FUNC_END();
                     return (_nVal - _start);
                 }
 
                 void __setNow() {
+                    PROFILER_FUNC_START();
                     std::chrono::microseconds ms = std::chrono::duration_cast<std::chrono::microseconds >(
                         std::chrono::high_resolution_clock::now().time_since_epoch()
                     );
+                    PROFILER_FUNC_END();
                     _start = ms.count();
                 }
         };
