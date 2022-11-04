4a01ff3e7cf599123af1f21d4720a818433f4f38
aureliancnx <aurelien.marcel@epitech.eu>
2022-11-04 15:14:47 +0100

    ADD: profiler for tools

--- KapEngine/Tools/Rectangle.cpp
+++ KapEngine/Tools/Rectangle.cpp
@@ -6,45 +6,63 @@
 */
 
 #include "Rectangle.hpp"
+#include "Profiler/KapProfiler.hpp"
 
 void KapEngine::Tools::Rectangle::setPos(Vector2 const& pos) {
+    PROFILER_FUNC_START();
     _pos = pos;
+    PROFILER_FUNC_END();
 }
 
 void KapEngine::Tools::Rectangle::setPos(float x, float y) {
+    PROFILER_FUNC_START();
     _pos.setX(x);
     _pos.setY(y);
+    PROFILER_FUNC_END();
 }
 
 void KapEngine::Tools::Rectangle::setSize(float w, float h) {
+    PROFILER_FUNC_START();
     _size.setX(w);
     _size.setY(h);
+    PROFILER_FUNC_END();
 }
 
 void KapEngine::Tools::Rectangle::setSize(Vector2 const& size) {
+    PROFILER_FUNC_START();
     _size = size;
+    PROFILER_FUNC_END();
 }
 
 void KapEngine::Tools::Rectangle::setH(float h) {
+    PROFILER_FUNC_START();
     _size.setY(h);
+    PROFILER_FUNC_END();
 }
 
 
 void KapEngine::Tools::Rectangle::setW(float w) {
+    PROFILER_FUNC_START();
     _size.setX(w);
+    PROFILER_FUNC_END();
 }
 
 
 void KapEngine::Tools::Rectangle::setX(float x) {
+    PROFILER_FUNC_START();
     _pos.setX(x);
+    PROFILER_FUNC_END();
 }
 
 
 void KapEngine::Tools::Rectangle::setY(float y) {
+    PROFILER_FUNC_START();
     _pos.setY(y);
+    PROFILER_FUNC_END();
 }
 
 std::string KapEngine::Tools::Rectangle::to_string() const {
+    PROFILER_FUNC_START();
     std::string res = "{";
 
     res += std::to_string(_pos.getX()) + "; ";
@@ -52,21 +70,30 @@ std::string KapEngine::Tools::Rectangle::to_string() const {
     res += std::to_string(_size.getX()) + "; ";
     res += std::to_string(_size.getY()) + "}";
 
+    PROFILER_FUNC_END();
     return res;
 }
 
 float KapEngine::Tools::Rectangle::getX() const {
+    PROFILER_FUNC_START();
+    PROFILER_FUNC_END();
     return getPos().getX();
 }
 
 float KapEngine::Tools::Rectangle::getY() const {
+    PROFILER_FUNC_START();
+    PROFILER_FUNC_END();
     return getPos().getY();
 }
 
 float KapEngine::Tools::Rectangle::getWidth() const {
+    PROFILER_FUNC_START();
+    PROFILER_FUNC_END();
     return getSize().getX();
 }
 
 float KapEngine::Tools::Rectangle::getHeigth() const {
+    PROFILER_FUNC_START();
+    PROFILER_FUNC_END();
     return getSize().getY();
 }
--- KapEngine/Tools/Vectors.cpp
+++ KapEngine/Tools/Vectors.cpp
@@ -6,51 +6,72 @@
 */
 
 #include "Vectors.hpp"
+#include "Profiler/KapProfiler.hpp"
 
 KapEngine::Tools::Vector2 KapEngine::Tools::Vector2::operator+(Vector3 const& vec) {
+    PROFILER_FUNC_START();
+    PROFILER_FUNC_END();
     return Vector2(x + vec.getX(), y + vec.getY());
 }
 
 KapEngine::Tools::Vector2 KapEngine::Tools::Vector2::operator-(Vector3 const& vec) {
+    PROFILER_FUNC_START();
+    PROFILER_FUNC_END();
     return Vector2(x - vec.getX(), y - vec.getY());
 }
 
 KapEngine::Tools::Vector2 KapEngine::Tools::Vector2::operator*(Vector3 const& vec) {
+    PROFILER_FUNC_START();
+    PROFILER_FUNC_END();
     return Vector2(x * vec.getX(), y * vec.getY());
 }
 
 KapEngine::Tools::Vector2 KapEngine::Tools::Vector2::operator/(Vector3 const& vec) {
+    PROFILER_FUNC_START();
+    PROFILER_FUNC_END();
     return Vector2(x / vec.getX(), y / vec.getY());
 }
 
 KapEngine::Tools::Vector2 &KapEngine::Tools::Vector2::operator=(Vector3 const& vec) {
+    PROFILER_FUNC_START();
     x = vec.getX();
     y = vec.getY();
 
+    PROFILER_FUNC_END();
     return *this;
 }
 
 
 KapEngine::Tools::Vector3 KapEngine::Tools::Vector3::operator+(Vector2 const& vec) {
+    PROFILER_FUNC_START();
+    PROFILER_FUNC_END();
     return Vector3(x + vec.getX(), y + vec.getY(), z);
 }
 
 KapEngine::Tools::Vector3 KapEngine::Tools::Vector3::operator-(Vector2 const& vec) {
+    PROFILER_FUNC_START();
+    PROFILER_FUNC_END();
     return Vector3(x - vec.getX(), y - vec.getY(), z);
 }
 
 KapEngine::Tools::Vector3 KapEngine::Tools::Vector3::operator*(Vector2 const& vec) {
+    PROFILER_FUNC_START();
+    PROFILER_FUNC_END();
     return Vector3(x * vec.getX(), y * vec.getY(), z);
 }
 
 KapEngine::Tools::Vector3 KapEngine::Tools::Vector3::operator/(Vector2 const& vec) {
+    PROFILER_FUNC_START();
+    PROFILER_FUNC_END();
     return Vector3(x / vec.getX(), y / vec.getY(), z);
 }
 
 KapEngine::Tools::Vector3 &KapEngine::Tools::Vector3::operator=(Vector2 const& vec) {
+    PROFILER_FUNC_START();
     x = vec.getX();
     y = vec.getY();
 
+    PROFILER_FUNC_END();
     return *this;
 }
 
