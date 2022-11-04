6fde055f1534e951c7eba25bc630297fb8615e7a
aureliancnx <aurelien.marcel@epitech.eu>
2022-11-04 15:14:39 +0100

    ADD: profiler for tools

--- KapEngine/Tools/Rectangle.hpp
+++ KapEngine/Tools/Rectangle.hpp
@@ -22,44 +22,66 @@ namespace KapEngine {
 
         class Rectangle {
             public:
-                Rectangle() {}
+                Rectangle() {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
+                }
                 Rectangle(float x, float y, float w, float h) {
+                    PROFILER_FUNC_START();
                     setPos(x, y);
                     setSize(w, h);
+                    PROFILER_FUNC_END();
                 }
                 Rectangle(Vector2 const& pos, Vector2 const& size) {
+                    PROFILER_FUNC_START();
                     setPos(pos);
                     setSize(size);
+                    PROFILER_FUNC_END();
                 }
-                ~Rectangle() {}
+                ~Rectangle() {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
+                }
 
                 void setPos(float x, float y);
                 void setPos(Vector2 const& pos);
                 void setX(float x);
                 void setPosX(float x) {
+                    PROFILER_FUNC_START();
                     setX(x);
+                    PROFILER_FUNC_END();
                 }
                 void setY(float y);
                 void setPosY(float y) {
+                    PROFILER_FUNC_START();
                     setY(y);
+                    PROFILER_FUNC_END();
                 }
 
                 void setSize(float width, float heigth);
                 void setSize(Vector2 const& size);
                 void setW(float w);
                 void setWidth(float w) {
+                    PROFILER_FUNC_START();
                     setW(w);
+                    PROFILER_FUNC_END();
                 }
                 void setH(float h);
                 void setHeigth(float h) {
+                    PROFILER_FUNC_START();
                     setH(h);
+                    PROFILER_FUNC_END();
                 }
 
                 Vector2 getPos() const {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return _pos;
                 }
 
                 Vector2 getSize() const {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return _size;
                 }
 
--- KapEngine/Tools/Vectors.hpp
+++ KapEngine/Tools/Vectors.hpp
@@ -24,31 +24,51 @@ namespace KapEngine {
         class Vector2 {
 
             public:
-                Vector2(float x = 0.0f, float y = 0.0f) : x(x), y(y) {}
-                Vector2(Vector2 const& vec) {
-                    *this = vec;
+                Vector2(float x = 0.0f, float y = 0.0f) : x(x), y(y) {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                 }
-                ~Vector2() {}
+                Vector2(Vector2 const& vec) {
+                    PROFILER_FUNC_START();
+                    *this = vec;
+                    PROFILER_FUNC_END();
+                }
+                ~Vector2() {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
+                }
 
                 void setX(float _x) {
+                    PROFILER_FUNC_START();
                     x = _x;
+                    PROFILER_FUNC_END();
                 }
                 void setY(float _y) {
+                    PROFILER_FUNC_START();
                     y = _y;
+                    PROFILER_FUNC_END();
                 }
 
                 float getX() const {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return x;
                 }
                 float getY() const {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return y;
                 }
 
                 Vector2 operator+(Vector2 const& vec) {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return Vector2(getX() + vec.getX(), y + vec.getY());
                 }
 
                 Vector2 operator+(float const& f) {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return Vector2(x + f, y + f);
                 }
 
@@ -56,26 +76,36 @@ namespace KapEngine {
 
 
                 Vector2 &operator+=(Vector2 const& vec) {
+                    PROFILER_FUNC_START();
                     *this = *this + vec;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
                 Vector2 &operator+=(float const& f) {
+                    PROFILER_FUNC_START();
                     *this = *this + f;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
                 Vector2 &operator+=(Vector3 const& vec) {
+                    PROFILER_FUNC_START();
                     *this = *this + vec;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
 
                 Vector2 operator-(Vector2 const& vec) {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return Vector2(x - vec.getX(), y - vec.getY());
                 }
 
                 Vector2 operator-(float const& f) {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return Vector2(x - f, y - f);
                 }
 
@@ -83,26 +113,36 @@ namespace KapEngine {
 
 
                 Vector2 &operator-=(Vector2 const& vec) {
+                    PROFILER_FUNC_START();
                     *this = *this - vec;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
                 Vector2 &operator-=(float const& f) {
+                    PROFILER_FUNC_START();
                     *this = *this - f;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
                 Vector2 &operator-=(Vector3 const& vec) {
+                    PROFILER_FUNC_START();
                     *this = *this - vec;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
 
                 Vector2 operator*(Vector2 const& vec) {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return Vector2(x * vec.getX(), y * vec.getY());
                 }
 
                 Vector2 operator*(float const& f) {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return Vector2(x * f, y * f);
                 }
 
@@ -110,26 +150,36 @@ namespace KapEngine {
 
 
                 Vector2 &operator*=(Vector2 const& vec) {
+                    PROFILER_FUNC_START();
                     *this = *this * vec;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
                 Vector2 &operator*=(float const& f) {
+                    PROFILER_FUNC_START();
                     *this = *this * f;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
                 Vector2 &operator*=(Vector3 const& vec) {
+                    PROFILER_FUNC_START();
                     *this = *this * vec;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
 
                 Vector2 operator/(Vector2 const& vec) {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return Vector2(x / vec.getX(), y / vec.getY());
                 }
 
                 Vector2 operator/(float const& f) {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return Vector2(x / f, y / f);
                 }
 
@@ -137,55 +187,76 @@ namespace KapEngine {
 
 
                 Vector2 &operator/=(Vector2 const& vec) {
+                    PROFILER_FUNC_START();
                     *this = *this / vec;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
                 Vector2 &operator/=(float const& f) {
+                    PROFILER_FUNC_START();
                     *this = *this / f;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
                 Vector2 &operator/=(Vector3 const& vec) {
+                    PROFILER_FUNC_START();
                     *this = *this / vec;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
 
                 Vector2 &operator=(Vector2 const& vec) {
+                    PROFILER_FUNC_START();
                     x = vec.getX();
                     y = vec.getY();
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
                 Vector2 &operator=(Vector3 const& vec);
 
                 bool operator==(Vector2 const& vec) {
-                    if (vec.getX() != x)
+                    PROFILER_FUNC_START();
+                    if (vec.getX() != x) {
+                        PROFILER_FUNC_END();
                         return false;
-                    if (vec.getY() != y)
+                    }
+                    if (vec.getY() != y) {
+                        PROFILER_FUNC_END();
                         return false;
+                    }
                     return true;
                 }
 
                 bool operator!=(Vector2 const& vec) {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return !(*this == vec);
                 }
 
                 static Vector2 zero() {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return Vector2(0.f, 0.f);
                 }
 
                 static Vector2 one() {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return Vector2(1.f, 1.f);
                 }
 
                 std::string to_string() const {
+                    PROFILER_FUNC_START();
                     std::string res = "{";
 
                     res += std::to_string(x) + "; ";
                     res += std::to_string(y) + "}";
 
+                    PROFILER_FUNC_END();
                     return res;
                 }
 
@@ -197,37 +268,58 @@ namespace KapEngine {
         class Vector3 {
 
             public:
-                Vector3(float x = 0.0f, float y = 0.0f, float z = 0.0f) : x(x), y(y), z(z) {}
+                Vector3(float x = 0.0f, float y = 0.0f, float z = 0.0f) : x(x), y(y), z(z) {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
+                }
                 Vector3(Vector3 const& vec) {
+                    PROFILER_FUNC_START();
                     *this = vec;
+                    PROFILER_FUNC_END();
                 }
                 ~Vector3() {}
 
                 void setX(float _x) {
+                    PROFILER_FUNC_START();
                     x = _x;
+                    PROFILER_FUNC_END();
                 }
                 void setY(float _y) {
+                    PROFILER_FUNC_START();
                     y = _y;
+                    PROFILER_FUNC_END();
                 }
                 void setZ(float _z) {
+                    PROFILER_FUNC_START();
                     z = _z;
+                    PROFILER_FUNC_END();
                 }
 
                 float getX() const {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return x;
                 }
                 float getY() const {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return y;
                 }
                 float getZ() const {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return z;
                 }
 
                 Vector3 operator+(Vector3 const& vec) {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return Vector3(x + vec.getX(), y + vec.getY(), z + vec.getZ());
                 }
 
                 Vector3 operator+(float const& f) {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return Vector3(x + f, y + f, z + f);
                 }
 
@@ -235,106 +327,144 @@ namespace KapEngine {
 
 
                 Vector3 &operator+=(Vector3 const& vec) {
+                    PROFILER_FUNC_START();
                     *this = *this + vec;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
                 Vector3 &operator+=(Vector2 const& vec) {
+                    PROFILER_FUNC_START();
                     *this = *this + vec;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
                 Vector3 &operator+=(float const& f) {
+                    PROFILER_FUNC_START();
                     *this = *this + f;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
 
                 Vector3 operator-(Vector3 const& vec) {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return Vector3(x - vec.getX(), y - vec.getY(), z - vec.getZ());
                 }
 
                 Vector3 operator-(Vector2 const& vec);
 
                 Vector3 operator-(float const& f) {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return Vector3(x - f, y - f, z - f);
                 }
 
 
                 Vector3 &operator-=(Vector3 const& vec) {
+                    PROFILER_FUNC_START();
                     *this = *this - vec;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
                 Vector3 &operator-=(Vector2 const& vec) {
+                    PROFILER_FUNC_START();
                     *this = *this - vec;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
                 Vector3 &operator-=(float const& f) {
+                    PROFILER_FUNC_START();
                     *this = *this - f;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
 
                 Vector3 operator*(Vector3 const& vec) {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return Vector3(x * vec.getX(), y * vec.getY(), z * vec.getZ());
                 }
 
                 Vector3 operator*(Vector2 const& vec);
 
                 Vector3 operator*(float const& f) {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return Vector3(x * f, y * f, z * f);
                 }
 
 
                 Vector3 &operator*=(Vector3 const& vec) {
+                    PROFILER_FUNC_START();
                     *this = *this * vec;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
                 
                 Vector3 &operator*=(Vector2 const& vec) {
+                    PROFILER_FUNC_START();
                     *this = *this * vec;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
                 Vector3 &operator*=(float const& f) {
+                    PROFILER_FUNC_START();
                     *this = *this * f;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
 
                 Vector3 operator/(Vector3 const& vec) {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return Vector3(x / vec.getX(), y / vec.getY(), z / vec.getZ());
                 }
 
                 Vector3 operator/(Vector2 const& vec);
 
                 Vector3 operator/(float const& f) {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return Vector3(x / f, y / f, z / f);
                 }
 
 
                 Vector3 &operator/=(Vector3 const& vec) {
+                    PROFILER_FUNC_START();
                     *this = *this / vec;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
                 Vector3 &operator/=(Vector2 const& vec) {
+                    PROFILER_FUNC_START();
                     *this = *this / vec;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
                 Vector3 &operator/=(float const& f) {
+                    PROFILER_FUNC_START();
                     *this = *this / f;
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
 
                 Vector3 &operator=(Vector3 const& vec) {
+                    PROFILER_FUNC_START();
                     x = vec.getX();
                     y = vec.getY();
                     z = vec.getZ();
+                    PROFILER_FUNC_END();
                     return *this;
                 }
 
@@ -342,34 +472,49 @@ namespace KapEngine {
 
 
                 bool operator==(Vector3 const& vec) {
-                    if (vec.getX() != x)
+                    PROFILER_FUNC_START();
+                    if (vec.getX() != x) {
+                        PROFILER_FUNC_END();
                         return false;
-                    if (vec.getY() != y)
+                    }
+                    if (vec.getY() != y) {
+                        PROFILER_FUNC_END();
                         return false;
-                    if (vec.getZ() != z)
+                    }
+                    if (vec.getZ() != z) {
+                        PROFILER_FUNC_END();
                         return false;
+                    }
                     return true;
                 }
 
                 bool operator!=(Vector3 const& vec) {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return !(*this == vec);
                 }
 
                 static Vector3 zero() {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return Vector3(0.f, 0.f, 0.f);
                 }
 
                 static Vector3 one() {
+                    PROFILER_FUNC_START();
+                    PROFILER_FUNC_END();
                     return Vector3(1.f, 1.f, 1.f);
                 }
 
                 std::string to_string() const {
+                    PROFILER_FUNC_START();
                     std::string res = "{";
 
                     res += std::to_string(x) + "; ";
                     res += std::to_string(y) + "; ";
                     res += std::to_string(z) + "}";
 
+                    PROFILER_FUNC_END();
                     return res;
                 }
 
