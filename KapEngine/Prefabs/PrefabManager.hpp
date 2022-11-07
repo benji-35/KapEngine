9e86e78f06afb4613f699e89fb3fdd6d445648ed
Chaika9 <bestdevelop22@gmail.com>
2022-11-07 21:53:38 +0100

    Add PrefabManager handler name of the Prefab in handler param

--- KapEngine/Prefabs/PrefabManager.hpp
+++ KapEngine/Prefabs/PrefabManager.hpp
@@ -35,19 +35,41 @@ namespace KapEngine {
             private:
             KEngine &_engine;
 
-            using Prefab = std::function<std::shared_ptr<GameObject>(SceneManagement::Scene&)>;
+            using Prefab = std::function<std::shared_ptr<GameObject>(SceneManagement::Scene&, const std::string&)>;
             Dictionary<std::string, std::shared_ptr<Prefab>> _prefabs;
 
             public:
-            PrefabManager(KEngine &engine);
+            explicit PrefabManager(KEngine &engine);
             ~PrefabManager() = default;
 
-            void createPrefab(std::string const& name, Prefab hander);
+            /**
+             * @brief Create a prefab
+             * @param name The name of the prefab
+             * @param hander The handler of the prefab (the function that will be called when the prefab is instantiated)
+             */
+            void createPrefab(std::string const& name, const Prefab& hander);
 
+            /**
+             * @brief Instantiate a prefab
+             * @param name The name of the prefab
+             * @param scene The scene where the prefab will be instantiated
+             * @param gameObject The game object that will be instantiated
+             */
             bool instantiatePrefab(std::string const& name, SceneManagement::Scene& scene, std::shared_ptr<GameObject>& gameObject);
 
+            /**
+             * @brief Instantiate a prefab
+             * @param name The name of the prefab
+             * @param sceneId The id of the scene where the prefab will be instantiated
+             * @param gameObject The game object that will be instantiated
+             * @param components The components that will be added to the game object
+             */
             bool instantiatePrefab(std::string const& name, std::size_t const& sceneId, std::shared_ptr<GameObject>& gameObject);
 
+            /**
+             * @brief Destroy a prefab
+             * @param name The name of the prefab
+             */
             void removePrefab(std::string const& name);
         };
     }
